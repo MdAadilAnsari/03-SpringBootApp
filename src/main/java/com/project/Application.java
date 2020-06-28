38df90732330389bfa289d8ffb20b3b913db1efc
Aadil <ansari.aadil863@gmail.com>
2020-06-28 12:05:56 +0530

    added rest-url

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	public static final String REST-URL = "https://www.firstcry.com/getshirt";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}

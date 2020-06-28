921f94aff73b75a49c98cf16db052efae4427f72
Aadil <ansari.aadil863@gmail.com>
2020-06-28 12:26:36 +0530

    HIS-301:added a variable

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static final String REST-URL = "https://www.firstcry.com/getshirt";
+	
 	public static void main(String[] args) {
+		int x =10;
 		ConfigurableApplicationContext ctxt = SpringApplication.run(Application.class, args);
 
 		ctxt.close();
